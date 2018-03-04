5dfd49fc06dd48cecf8a6f79f869009715c77205
Eibhlin McGeady <eibhlin.mc.geady@accenture.com>
2018-03-04 20:27:44 +0000

    add routing

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header" />
+    );
+  }
+}
+export default Header;
