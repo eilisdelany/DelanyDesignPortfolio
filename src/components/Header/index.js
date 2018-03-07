8c657242b614065387597121c59bdce85b57c55a
Eibhlin McGeady <eibhlin.mc.geady@accenture.com>
2018-03-07 16:10:07 +0000

    Header Component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import { pages, pageKeys } from '../../constants/pages';
+import './index.scss';
 
 class Header extends Component {
   render() {
+    const activePath = this.props.match.path;
     return (
-      <div className="Header" />
+      <div className="Header">
+        <img
+          className="Header-logo"
+          alt="eyelash logo"
+          src="/images/logo.png"
+        />
+        <div className="Header-tabs">
+          {pageKeys.map((pageKey, index) => {
+            const { path, title } = pages[pageKey];
+            return (
+              <div key={index} className={path === activePath ? "Header-active-tab" : "Header-tab"}>
+                <NavLink to={path}>
+                  {title}
+                </NavLink>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     );
   }
 }
