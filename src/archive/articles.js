5b3c93481ee0402203bf1d0523386805e8000c90
Eibhlin McGeady <eibhlin.mc.geady@accenture.com>
2018-03-12 19:23:30 +0000

    mock articles archive

---
+++ src/archive/articles.js
@@ -0,0 +1,31 @@
+export default [{
+  date: "12.03.18",
+  files: [{
+    text: "This is a lovely wee article I read",
+    url: "https://www.google.com"
+  }, {
+    text: "This is another lovely wee article I read",
+    url: "https://www.google.com"
+  }, {
+    text: "This one is ok",
+    url: "https://www.google.com"
+  }, {
+    text: "This is another.",
+    url: "https://www.google.com"
+  }]
+}, {
+  date: "05.03.18",
+  files: [{
+    text: "This is a lovely wee article I read",
+    url: "https://www.google.com"
+  }, {
+    text: "This is another lovely wee article I read",
+    url: "https://www.google.com"
+  }, {
+    text: "This one is ok",
+    url: "https://www.google.com"
+  }, {
+    text: "This is another.",
+    url: "https://www.google.com"
+  }]
+}];
