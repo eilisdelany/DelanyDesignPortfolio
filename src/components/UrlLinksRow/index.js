5d763e0e2e3707bc67a0ecb29d53f7be0cc0fe00
Eibhlin McGeady <eibhlin.mc.geady@accenture.com>
2018-03-12 19:24:12 +0000

    url links row component

---
+++ src/components/UrlLinksRow/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './index.scss';
+
+class UrlLinksRow extends Component {
+
+  render() {
+    const { date, files } = this.props;
+    return (
+      <div className="UrlLinksRow">
+        <div className="UrlLinksRow-date-col">
+          {`-- ${date}`}
+        </div>
+        <div className="UrlLinksRow-links-col">
+          {
+            files.map(({ text, url }, index) => {
+              return <a key={index} href={url} target="_blank" className="UrlLinksRow-link">{text}</a>
+            })
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+UrlLinksRow.propTypes = {
+  date: PropTypes.string.isRequired,
+  files: PropTypes.array.isRequired
+}
+export default UrlLinksRow;
