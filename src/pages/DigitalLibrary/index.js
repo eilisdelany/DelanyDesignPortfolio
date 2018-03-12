86e61f129d56602f064b19901dfbe0921d8b8209
Eibhlin McGeady <eibhlin.mc.geady@accenture.com>
2018-03-12 17:55:28 +0000

    pages with ComingSoon section

---
+++ src/pages/DigitalLibrary/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ComingSoonSection from '../../components/ComingSoonSection';
+import './index.scss';
+
+class DigitalLibrary extends Component {
+  render() {
+    return (
+      <div className="DigitalLibrary">
+        <ComingSoonSection />
+      </div>
+    );
+  }
+}
+export default DigitalLibrary;
