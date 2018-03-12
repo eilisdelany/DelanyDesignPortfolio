17d7f4bc1c03fba5a4415c494affac0048a03af7
Eibhlin McGeady <eibhlin.mc.geady@accenture.com>
2018-03-12 17:55:39 +0000

    add all routes to app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import {BrowserRouter, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import ArticlesArchive from './pages/ArticlesArchive';
+import BlogArchive from './pages/BlogArchive';
+import DigitalLibrary from './pages/DigitalLibrary';
+import Contact from './pages/Contact';
+
 import { pages } from './constants/pages';
 import './App.scss';
 
@@ -16,6 +21,11 @@ class App extends Component {
             </header>
             <div className="App-content">
               <Route exact path={pages.ITS_ME.path} component={Home} />
+              <Route exact path={pages.TOP_FIVE.path} component={ArticlesArchive} />
+              <Route exact path={pages.WORDS.path} component={BlogArchive} />
+              <Route exact path={pages.HIT_LIST.path} component={DigitalLibrary} />
+              <Route exact path={pages.LETS_TALK.path} component={Contact} />
+
             </div>
             <footer>
               <Route path="/" component={Footer} />
