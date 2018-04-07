8ec2ac615c5dbac7655b448687370fa4b465eadd
Eibhlin McGeady <eibhlin.mc.geady@accenture.com>
2018-04-07 14:37:18 +0100

    Add catalogue page

---
+++ src/pages/Catalogue/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ComingSoonSection from '../../components/ComingSoonSection';
+
+class Catalogue extends Component {
+  render() {
+    return (
+      <div className="Catagloue">
+        <ComingSoonSection />
+      </div>
+    );
+  }
+}
+export default Catalogue;
