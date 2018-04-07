2256c23f71a15380e5d72d2543b6b9bdf5e4a03f
Eibhlin McGeady <eibhlin.mc.geady@accenture.com>
2018-04-07 15:34:59 +0100

    intro section page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,7 +4,16 @@ import './index.scss';
 class Home extends Component {
   render() {
     return (
-      <div className="Home" />
+      <div className="Home">
+        <section className="Home-intro-section">
+          <h3>{'Introductions can be awkward.'}</h3>
+          <h2>
+            {'Particularly if you have an Irish name which sounds like '}
+            <img src="/images/eyelash.png" alt="eyelash" />
+          </h2>
+          <span>{'Wanna scroll past the small talk?'}</span>
+        </section>
+      </div>
     );
   }
 }
