696180c2a1e505924929c64a618c70b27a804d7f
Eibhlin McGeady <eibhlin.mc.geady@accenture.com>
2018-04-07 15:44:17 +0100

    create new component and update styles

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
+import IntroSection from './IntroSection'
 import './index.scss';
 
 class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <section className="Home-intro-section">
-          <h3>{'Introductions can be awkward.'}</h3>
-          <h2>
-            {'Particularly if you have an Irish name which sounds like '}
-            <img src="/images/eyelash.png" alt="eyelash" />
-          </h2>
-          <span>{'Wanna scroll past the small talk?'}</span>
-        </section>
+        <IntroSection />
       </div>
     );
   }
