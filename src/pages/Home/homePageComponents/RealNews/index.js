5061982d1b0525ad92b6ef24b869be644855bb8e
Eibhlin McGeady <eibhlin.mc.geady@accenture.com>
2018-04-29 16:32:33 +0100

    real news component

--- src/pages/Home/homePageComponents/RealNews/index.js
+++ src/pages/Home/homePageComponents/RealNews/index.js
@@ -1,15 +1,40 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import ComingSoonSection from '../../../../components/ComingSoonSection';
+import PageTitle from "../../../../components/PageTitle";
+import ContentLayout from "../../../../components/ContentLayout";
+import newsArticles from "../../../../archive/news-articles";
+import "./index.scss";
 
 class RealNews extends Component {
   render() {
-    return <ComingSoonSection />;
+    return (
+      <section className="RealNews">
+        <PageTitle
+          text={`I'm not looking to get elected anytime soon so all my news is real.`}
+        />
+        <ContentLayout
+          subtitle={newsArticles.title}
+          content={newsArticles.files.map(({ text, source, url }, index) => {
+            return (
+              <div key={index} className="RealNews-row">
+                <span>{text}</span>
+                <a
+                  href={url}
+                  target="_blank"
+                >
+                  {source}
+                </a>
+              </div>
+            );
+          })}
+        />
+      </section>
+    );
   }
 }
 
 RealNews.propTypes = {
-  goBack: PropTypes.string.isRequired
+  goBack: PropTypes.func.isRequired
 };
 
 export default RealNews;
