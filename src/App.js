3bbdb6a93377916d0c9dba8a8242bd70f798aeb4
Eibhlin McGeady <eibhlin.mc.geady@accenture.com>
2018-05-14 08:27:54 +0100

    set up aos

--- src/App.js
+++ src/App.js
@@ -1,83 +1,84 @@
-import React, { Component } from 'react';
-import {BrowserRouter, Route, Switch } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Catalogue from './pages/Catalogue';
-import Archive from './pages/Archive';
-import DigitalLibrary from './pages/DigitalLibrary';
-import Contact from './pages/Contact';
-import ErrorPage from './pages/ErrorPage';
-import Words from './components/Words';
-import TopFive from './components/TopFive';
-import blogPosts from './archive/blog-posts';
-import topFive from './archive/top-five';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Catalogue from "./pages/Catalogue";
+import Archive from "./pages/Archive";
+import DigitalLibrary from "./pages/DigitalLibrary";
+import Contact from "./pages/Contact";
+import ErrorPage from "./pages/ErrorPage";
+import Words from "./components/Words";
+import TopFive from "./components/TopFive";
+import blogPosts from "./archive/blog-posts";
+import topFive from "./archive/top-five";
 
-import { pages } from './constants/pages';
-import './App.scss';
+import { pages } from "./constants/pages";
+import "./App.scss";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-            <header>
-              <Route path="/" component={Header} />
-            </header>
-            <div className="App-content">
-              <Switch>
-                <Route exact path={pages.ITS_ME.path} component={Home} />
-                <Route exact path={pages.CATALOGUE.path} component={Catalogue} />
-                <Route
-                  exact
-                  path={pages.TOP_FIVE.path}
-                  render={(props) => (
-                    <Archive
-                      pageTitleText={pages.TOP_FIVE.pageTitle}
-                      archive={topFive}
-                      Content={TopFive}
-                      {...props}
-                    />
-                  )}
-                />
-                <Route
-                  exact
-                  path={pages.WORDS.path}
-                  render={(props) => (
-                    <Archive
-                      pageTitleText={pages.WORDS.pageTitle}
-                      archive={blogPosts}
-                      Content={Words}
-                      {...props}
-                    />
-                  )}
-                />
-                <Route
-                  exact
-                  path={pages.HIT_LIST.path}
-                  render={(props) => (
-                    <DigitalLibrary
-                      pageTitleText={pages.HIT_LIST.pageTitle}
-                      {...props}
-                    />
-                  )}
-                />
-                <Route
-                  exact
-                  path={pages.LETS_TALK.path}
-                  render={(props) => (
-                    <Contact
-                      pageTitleText={pages.LETS_TALK.pageTitle}
-                      {...props}
-                    />
-                  )}
-                />
-                <Route component={ErrorPage} />
-              </Switch>
-            </div>
-            <footer>
-              <Route path="/" component={Footer} />
-            </footer>
+          <link rel="stylesheet" href="bower_components/aos/dist/aos.css" />
+          <header>
+            <Route path="/" component={Header} />
+          </header>
+          <div className="App-content">
+            <Switch>
+              <Route exact path={pages.ITS_ME.path} component={Home} />
+              <Route exact path={pages.CATALOGUE.path} component={Catalogue} />
+              <Route
+                exact
+                path={pages.TOP_FIVE.path}
+                render={props => (
+                  <Archive
+                    pageTitleText={pages.TOP_FIVE.pageTitle}
+                    archive={topFive}
+                    Content={TopFive}
+                    {...props}
+                  />
+                )}
+              />
+              <Route
+                exact
+                path={pages.WORDS.path}
+                render={props => (
+                  <Archive
+                    pageTitleText={pages.WORDS.pageTitle}
+                    archive={blogPosts}
+                    Content={Words}
+                    {...props}
+                  />
+                )}
+              />
+              <Route
+                exact
+                path={pages.HIT_LIST.path}
+                render={props => (
+                  <DigitalLibrary
+                    pageTitleText={pages.HIT_LIST.pageTitle}
+                    {...props}
+                  />
+                )}
+              />
+              <Route
+                exact
+                path={pages.LETS_TALK.path}
+                render={props => (
+                  <Contact
+                    pageTitleText={pages.LETS_TALK.pageTitle}
+                    {...props}
+                  />
+                )}
+              />
+              <Route component={ErrorPage} />
+            </Switch>
+          </div>
+          <footer>
+            <Route path="/" component={Footer} />
+          </footer>
         </div>
       </BrowserRouter>
     );
--- src/pages/DigitalLibrary/index.js
+++ src/pages/DigitalLibrary/index.js
@@ -1,12 +1,23 @@
-import React, { Component } from 'react';
-import ComingSoonSection from '../../components/ComingSoonSection';
-import './index.scss';
+import React, { Component } from "react";
+import AOS from "aos";
+import PageTitle from "../../components/PageTitle";
+import "./index.scss";
 
 class DigitalLibrary extends Component {
+  componentDidMount() {
+    AOS.init();
+  }
   render() {
     return (
       <div className="DigitalLibrary">
-        <ComingSoonSection />
+        <PageTitle text="My personal library is in constant flux and never expands beyond ten books at a given time. I want this web page to act as a visual diary for those looking for more than bite-sized articles on digital devices." />
+        <img
+          href={
+            "https://www.bookbind.net/Leather-Slip-On-Style-REGULAR-BIBLE-Book-Cover-p/b-sod-30h.htm"
+          }
+          alt="bible"
+          data-aos="fade-right"
+        />
       </div>
     );
   }
