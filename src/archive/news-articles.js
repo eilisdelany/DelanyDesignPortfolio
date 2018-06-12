37b3b4ba13acbe6767271c7ff9ad542678a03ae4
Eibhlin McGeady <eibhlin.mc.geady@accenture.com>
2018-06-12 21:27:16 +0100

    add news article

--- src/archive/news-articles.js
+++ src/archive/news-articles.js
@@ -1,32 +1,52 @@
 export default {
   title: "Real News",
-  files: [{
-    text: "How Did I get Here?",
-    source: "Sketch Labs",
-    url: "https://jobs.sketchlabs.io/blogposts/how-did-i-get-here-eilis-delany "
-  }, {
-    text: "Grown-up, scientist designers",
-    source: "Nationwide / RTE",
-    url: "https://www.rte.ie/news/player/nationwide/2016/0106/"
-  }, {
-    text: "Irelands Inventors",
-    source: "Today FM",
-    url: "http://www.todayfm.com/Irelands-inventors--"
-  }, {
-    text: "Two Irish inventors shortlisted for James Dyson Award for £37,000 prize",
-    source: "Silicon Republic",
-    url: "https://www.siliconrepublic.com/discovery/james-dyson-award-irish-inventors"
-  }, {
-    text: "Two Irish students on shortlist for Dyson award",
-    source: "The Irish Times",
-    url: "https://www.irishtimes.com/business/technology/two-irish-students-on-shortlist-for-dyson-award-1.2354430"
-  }, {
-    text: "Two Irish students on shortlist for Dyson award",
-    source: "Irish Independent",
-    url: " https://www.independent.ie/irish-news/education/two-irish-graduates-in-the-running-for-international-invention-prize-34181084.html"
-  }, {
-    text: "Young Irish Inventors make James Dyson award shortlist",
-    source: "Irish Examiner",
-    url: "http://www.irishexaminer.com/ireland/young-irish-inventors-make-james-dyson-award-shortlist-354485.html"
-  }]
+  files: [
+    {
+      text: "How Did I get Here?",
+      source: "Sketch Labs",
+      url:
+        "https://jobs.sketchlabs.io/blogposts/how-did-i-get-here-eilis-delany "
+    },
+    {
+      text: "Grown-up, scientist designers",
+      source: "Nationwide / RTE",
+      url: "https://www.rte.ie/news/player/nationwide/2016/0106/"
+    },
+    {
+      text: "Irelands Inventors",
+      source: "Today FM",
+      url: "http://www.todayfm.com/Irelands-inventors--"
+    },
+    {
+      text:
+        "Two Irish inventors shortlisted for James Dyson Award for £37,000 prize",
+      source: "Silicon Republic",
+      url:
+        "https://www.siliconrepublic.com/discovery/james-dyson-award-irish-inventors"
+    },
+    {
+      text: "Two Irish students on shortlist for Dyson award",
+      source: "The Irish Times",
+      url:
+        "https://www.irishtimes.com/business/technology/two-irish-students-on-shortlist-for-dyson-award-1.2354430"
+    },
+    {
+      text: "Two Irish students on shortlist for Dyson award",
+      source: "Irish Independent",
+      url:
+        " https://www.independent.ie/irish-news/education/two-irish-graduates-in-the-running-for-international-invention-prize-34181084.html"
+    },
+    {
+      text: "Young Irish Inventors make James Dyson award shortlist",
+      source: "Irish Examiner",
+      url:
+        "http://www.irishexaminer.com/ireland/young-irish-inventors-make-james-dyson-award-shortlist-354485.html"
+    },
+    {
+      text: "Episode 23: Eilís talks Business Design",
+      source: "GradLife Podcast",
+      url:
+        "https://itunes.apple.com/ie/podcast/episode-23-business-design/id1249668733?i=1000413079823&mt=2"
+    }
+  ]
 };
