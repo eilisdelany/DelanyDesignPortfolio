805353f86985ea7c926783d3b683bf6fb96028f1
Eibhlin McGeady <eibhlin.mc.geady@accenture.com>
2018-06-13 09:10:25 +0100

    move home page components to their own routes

--- src/constants/pages.js
+++ src/constants/pages.js
@@ -1,31 +1,42 @@
 export const pages = {
   ITS_ME: {
-    path: '/',
-    title: 'It\'s me'
+    path: "/",
+    title: "It's me",
+    subpages: {
+      MANTRA: "/mantra",
+      GRINDIN: "/grindin",
+      SAY_WHAT: "/say-what",
+      REAL_NEWS: "/real-news",
+      PINCH_ME: "/pinch-me",
+      DREAM_DINNER: "/dream-dinner"
+    }
   },
   CATALOGUE: {
-    path: '/archive',
-    title: 'archive'
+    path: "/archive",
+    title: "archive"
   },
   TOP_FIVE: {
-    path: '/top-five',
-    title: 'Top five',
-    pageTitle: 'This is my digital treasure trove - a curated collection of links covering science, technology, design and personal growth.'
+    path: "/top-five",
+    title: "Top five",
+    pageTitle:
+      "This is my digital treasure trove - a curated collection of links covering science, technology, design and personal growth."
   },
   WORDS: {
-    path: '/words',
-    title: 'Words',
-    pageTitle: 'Now and again, I share my point of view. Some people care, some people don\'t.'
+    path: "/words",
+    title: "Words",
+    pageTitle:
+      "Now and again, I share my point of view. Some people care, some people don't."
   },
   HIT_LIST: {
-    path: '/hit-list',
-    title: 'Hit list',
-    pageTitle: 'My personal library consists of fifteen books, none of which sit on a shelf untouched. Each one is bought to serve a purpose and when complete, is passed on to a friend. I want this page to act as a visual diary for those looking for more than bite sized articles on digital devices.'
+    path: "/hit-list",
+    title: "Hit list",
+    pageTitle:
+      "My personal library consists of fifteen books, none of which sit on a shelf untouched. Each one is bought to serve a purpose and when complete, is passed on to a friend. I want this page to act as a visual diary for those looking for more than bite sized articles on digital devices."
   },
   LETS_TALK: {
-  path: '/lets-talk',
-  title: 'Let\'s talk',
-  pageTitle: 'What\'s it gonna be?'
+    path: "/lets-talk",
+    title: "Let's talk",
+    pageTitle: "What's it gonna be?"
   }
 };
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,53 +2,25 @@ import React, { Component } from "react";
 import IntroSection from "./IntroSection";
 import TeaserSection from "./TeaserSection";
 import FibonacciSection from "./FibonacciSection";
-import homePageComponents from "../../constants/homePageComponents";
-import Mantra from "./homePageComponents/Mantra";
-import Timeline from "./homePageComponents/Timeline";
-import SayWhat from "./homePageComponents/SayWhat";
-import RealNews from "./homePageComponents/RealNews";
-import PinchMe from "./homePageComponents/PinchMe";
-import DreamDinner from "./homePageComponents/DreamDinner";
 import "./index.scss";
 
+const componentId = "fibonacci-section";
+
 class Home extends Component {
-  constructor() {
-    super();
-    this.state = {
-      selectedHomeComponent: undefined
-    };
+  componentDidMount() {
+    if (this.props.location.state && this.props.location.state.scrollToComponent) {
+      document.getElementById(componentId).scrollIntoView();
+    }
   }
 
-  updateSelectedHomeComponent = (selectedHomeComponent = undefined) => {
-    window.scrollTo(0,0);
-    this.setState({ selectedHomeComponent });
-  };
-
   render() {
-    switch (this.state.selectedHomeComponent) {
-      case homePageComponents.MANTRA:
-        return <Mantra goBack={this.updateSelectedHomeComponent} />;
-      case homePageComponents.GRINDIN:
-        return <Timeline goBack={this.updateSelectedHomeComponent} />;
-      case homePageComponents.SAY_WHAT:
-        return <SayWhat goBack={this.updateSelectedHomeComponent} />;
-      case homePageComponents.REAL_NEWS:
-        return <RealNews goBack={this.updateSelectedHomeComponent} />;
-      case homePageComponents.PINCH_ME:
-        return <PinchMe goBack={this.updateSelectedHomeComponent} />;
-      case homePageComponents.DREAM_DINNER:
-        return <DreamDinner goBack={this.updateSelectedHomeComponent} />;
-      default:
-        return (
-          <div className="Home">
-            <IntroSection />
-            <TeaserSection />
-            <FibonacciSection
-              selectComponent={this.updateSelectedHomeComponent}
-            />
-          </div>
-        );
-    }
+    return (
+      <div className="Home">
+        <IntroSection />
+        <TeaserSection />
+        <FibonacciSection componentId={componentId} />
+      </div>
+    );
   }
 }
 export default Home;
