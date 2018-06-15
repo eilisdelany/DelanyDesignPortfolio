db7f6fe7c089689bed6a146f8a139496c051da00
Eibhlin McGeady <eibhlin.mc.geady@accenture.com>
2018-06-15 09:38:01 +0100

    TextImage component

---
+++ src/pages/Catalogue/Project/ProjectBody/ProjectComponents/TextImage/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "../index.scss";
+
+class TextImage extends Component {
+  render() {
+    const { position, title, text, image, caption } = this.props;
+    return (
+      <section className={`TextImage position position-${position}`}>
+        <div className="image">
+          <img src={`/images/catalogue/${image}.png`} alt={caption} />
+          <span className="img-caption">{caption}</span>
+        </div>
+        <div className="content">
+          <h4 className="title">{`- ${title}`}</h4>
+          <span className="text">{text}</span>
+        </div>
+      </section>
+    );
+  }
+}
+TextImage.propTypes = {
+  position: PropTypes.oneOf(["left", "right"]).isRequired,
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  caption: PropTypes.string.isRequired
+};
+
+export default TextImage;
