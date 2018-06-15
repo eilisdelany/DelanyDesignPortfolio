7479d736cde8dbab47c7a8949298b39fbc24ba88
Eibhlin McGeady <eibhlin.mc.geady@accenture.com>
2018-06-15 09:38:09 +0100

    quote component

---
+++ src/pages/Catalogue/Project/ProjectBody/ProjectComponents/Quote/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "../index.scss";
+
+class Quote extends Component {
+  render() {
+    const { position, text, name } = this.props;
+    return (
+      <section className={`Quote position position-${position}`}>
+        <div>
+          <span className="text">{text}</span>
+          <span className="name">{name}</span>
+        </div>
+      </section>
+    );
+  }
+}
+Quote.propTypes = {
+  position: PropTypes.oneOf(["left", "right"]).isRequired,
+  text: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+export default Quote;
