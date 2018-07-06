e0e27167c750b06f02c5354785359519981eb07e
Eibhlin McGeady <eibhlin.mc.geady@accenture.com>
2018-07-06 10:26:22 +0100

    add images and update content

--- src/archive/catalogue/index.js
+++ src/archive/catalogue/index.js
@@ -20,70 +20,80 @@ export default [
           position: "right",
           title: "Background",
           text:
-            "Nothing is ever stratightforward so explore alternative ways to proceed and don’t overcomplicate the problem (or solution). \n\nNothing is ever stratightforward so explore alternative ways to proceed and don’t overcomplicate the problem (or solution). \n\nNothing is ever stratightforward so explore alternative ways to proceed and don’t overcomplicate the problem (or solution). ",
+            "Artificial Intelligence is more than just a technology.\n\nBeyond the algorithms, AI drives services that are rapidly changing our work and personal lives. \n\nAs innovators, this requires us to consider its wider implications and be accountable for how we want people to interact with AI, but also, each other. \n\nIf interdisciplianry teams don’t align on terms or processes, we will never be able to stand behind such “intelligent” systems.",
           image: "img_1",
-          caption: "Create something something"
+          caption:
+            "Collaboration requires respect: Connor Upton, Director at Fjord."
         },
         {
           type: types.IMG_TEXT,
           position: "left",
           title: "How might we?",
           text:
-            "Nothing is ever stratightforward so explore alternative ways to proceed and don’t overcomplicate the problem (or solution). \n\nNothing is ever stratightforward so explore alternative ways to proceed and don’t overcomplicate the problem (or solution). \n\nNothing is ever stratightforward so explore alternative ways to proceed and don’t overcomplicate the problem (or solution). ",
+            "How Might We develop content that explores Human-AI collaboration? \n\nWe wanted our workshop m​​aterial to be: \n- Context-aware: flexible content which could be tailored to suit different disciplines and levels of advancement, depending on attendees\n- Instantly applicable: practical activites and templates which could be used on live projects after the workshop \n- Collaborative: open format to encourage new methods and / or approaches ",
           image: "img_2",
-          caption: "Create something something"
+          caption: "Dynamic teams ideating in Dublin."
         },
         {
           type: types.IMG_TEXT,
           position: "right",
           title: "Challenges",
           text:
-            "Nothing is ever stratightforward so explore alternative ways to proceed and don’t overcomplicate the problem (or solution). \n\nNothing is ever stratightforward so explore alternative ways to proceed and don’t overcomplicate the problem (or solution). \n\nNothing is ever stratightforward so explore alternative ways to proceed and don’t overcomplicate the problem (or solution). ",
+            "The key challenges for this project were:\n\n - Avoiding bias: we reached out to various experts at The Dock to ensure our content encompassed multiple perspectives and approaches \n - Trial and error: all content was original, meaning we had to create new methods and activities from scratch - not always successful \n- Structure and suspense: balancing theoretical lessons, project learnings and demos, group activities etc.",
           image: "img_3",
-          caption: "Create something something"
+          caption:
+            " Learning from the experts: Gaurav Kaila, Analytics Specialist."
         },
         {
           type: types.QUOTE,
           position: "right",
-          text: "“Launching something cool to benefit someone in need.”",
-          name: "Name of Person"
+          text:
+            "Eilís provided valuable insight into how to creatively communicate somewhat dry / technical AI concepts to a design audience and showed her inventive talent in translating them into engaging practical activities.",
+          name: "Jivan Virdee, Data Designer, Fjord London"
         },
         {
           type: types.VIDEO,
           position: "left",
           url: "https://www.youtube.com/embed/IR-4n_wwOSY",
-          caption: "Create Something Something"
+          caption:
+            "Sharing some project learnings from AI in Content Moderation."
         },
         {
           type: types.SLIDESHOW_TEXT,
           position: "left",
           title: "Approach",
           text:
-            "Nothing is ever stratightforward so explore alternative ways to proceed and don’t overcomplicate the problem (or solution). \n\nNothing is ever stratightforward so explore alternative ways to proceed and don’t overcomplicate the problem (or solution). \n\nNothing is ever stratightforward so explore alternative ways to proceed and don’t overcomplicate the problem (or solution). ",
+            "Over the past few months, this workshop has been delivered to teams and clients in Dublin, London, Stockholm and Melbourne. \n\nThe format changes with each group: a full-day workshop, a series of micro-lectures over the course of a few days, a single workshop activity etc. \n\nWe make a conscios effort to invite experts into the workshop to share their insights and learnings to ensure balance and accuracy. ",
           images: ["img_4", "img_5", "img_6", "img_7"],
-          caption: "Create Something Something"
+          caption: "Planning: Gemma Gallagher, Interaction Designer at Fjord."
         },
         {
           type: types.IMG_TEXT,
           position: "right",
           title: "Solution",
           text:
-            "Nothing is ever stratightforward so explore alternative ways to proceed and don’t overcomplicate the problem (or solution). \n\nNothing is ever stratightforward so explore alternative ways to proceed and don’t overcomplicate the problem (or solution). \n\nNothing is ever stratightforward so explore alternative ways to proceed and don’t overcomplicate the problem (or solution). ",
+            "Current agenda: \n- AI 101: misconceptions, practical applications, umbrella technology.\n - What, not how: the problem not the technology, combining capabilities\n - Human-AI collaboration: centaur systems, relationships and approaches \n - Guidelines and principles: interactions, teammate qualities, considerations \n - Aligning terminology: levels of AI, language",
           image: "img_8",
-          caption: "Create something something"
+          caption:
+            "Groups combine several AI techniques to create one robust solution."
         },
         {
           type: types.IMG_VIDEO,
           position: "left",
-          image: "img_9",
-          caption: "Create something something",
-          url: "https://www.youtube.com/embed/agKvdGN02Uo",
+          image: {
+            src: "img_9",
+            caption: "The real-life challenge: a grocery store."
+          },
+          url: {
+            src: "https://www.youtube.com/embed/agKvdGN02Uo",
+            caption: "Group role-play: the journey of a tomato."
+          },
         },
         {
           type: types.QUOTE,
           position: "left",
           text:
-            "“Thank you so much for today. It was excellent - extremely relevant to so much of our client work and such a unique Dock-like multidisciplinary angle.”",
+            "Thank you so much for today. It was excellent - extremely relevant to so much of our client work and such a unique Dock-like multidisciplinary angle.",
           name: "Eva Magure, Innovation Nexus Lead, Accenture."
         },
         {
@@ -99,7 +109,7 @@ export default [
             {
               title: "Feedback",
               text:
-                "“I wanted to reach out at the end of the week to say how much I enjoyed and learned from the Human Centric AI workshop this week. I really enjoyed the format and content.” \nJulie Spillane, Director, The Dock.\n\n“Fantastic workshop this morning, guys. Very clear, deep thinking presented in a very concise and understandable way. Delighted I was able to attend.”\nDonnacha, Service Design Lead, Fjord."
+                "“Eilís, was instrumental to the design and rollout of Fjords new workshop on Design & AI. Through her work, we have developed a unique asset that has supported training, thought leadership and client engagement.” \nConnor Upton, Director, Fjord.\n\n“Really pleased with the Dublin team for designing and facilitating this workshop on AI. The content is difficult and often misunderstood. I recieved a lot of positive feedback from the team here in London with requests for more information in certain areas”\nRichard Wiltshire, Principal Design Director for EALA, Fjord."
             }
           ]
         }
