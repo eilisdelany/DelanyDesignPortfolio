2a5c751e4c5351c13f9e1fe24ade08a70aa1e89f
Eibhlin McGeady <eibhlin.mc.geady@accenture.com>
2018-07-10 21:21:06 +0100

    add bullet points

--- src/pages/Catalogue/Project/ProjectBody/ProjectComponents/Text/index.js
+++ src/pages/Catalogue/Project/ProjectBody/ProjectComponents/Text/index.js
@@ -2,20 +2,42 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "../index.scss";
 
+const BulletPointList = ({ list }) => {
+  return (
+    <ul className="BulletPointList">
+      {list.map((listItem, index) => {
+        return <li key={index}>{listItem}</li>;
+      })}
+    </ul>
+  );
+};
+
 class Text extends Component {
   render() {
     const { title, text } = this.props;
     return (
       <div className="Text">
         <h4 className="title">{`- ${title}`}</h4>
-        <span className="text">{text}</span>
+
+        <span className="text">
+          {text instanceof Object
+            ? Object.keys(text).map((key, index) => {
+                if (key === "text") {
+                  return <p key={index}>{text[key]}</p>;
+                } else if (key === "list") {
+                  return <BulletPointList key={index} list={text[key]} />;
+                }
+                return null;
+              })
+            : text}
+        </span>
       </div>
     );
   }
 }
 Text.propTypes = {
   title: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired
+  text: PropTypes.oneOfType(PropTypes.string, PropTypes.object).isRequired
 };
 
 export default Text;
