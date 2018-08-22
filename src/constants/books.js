2899b5a04751c0822676800d6d79205bacc2f9da
Eibhlin McGeady <eibhlin.mc.geady@accenture.com>
2018-08-22 15:25:59 +0100

    add books descriptions

--- src/constants/books.js
+++ src/constants/books.js
@@ -2,185 +2,328 @@ export default [
   {
     id: "let-my-people-go-surfing",
     name: "Let my People go Surfing",
-    summary: "",
-    offsetXMax: "0px",
-    offsetXMin: "-100px",
-    offsetYMax: "200px",
-    offsetYMin: "0px",
+    author: "Yvon Chouinard, Naomi Klein",
+    tags: ["Business", "Entrepreneurship", "Sustainability"],
+    summary:
+      "This, for me, was a book about balance and fulfilment on a communal level. \n\nIt's humble and honest - Yvon doesn't uses complex businesses jargon or shy away from the mistakes he made while building up Patagonia, one of the largest (and most sustainable) apparel corporations in the world today.",
+    reference:
+      "http://www.patagonia.com/product/let-my-people-go-surfing-revised-paperback-book/BK067.html",
+    offsets: {
+      xMax: "0px",
+      xMin: "-100px",
+      yMax: "200px",
+      yMin: "0px"
+    }
   },
   {
     id: "holo-2",
     name: "Holo 2",
-    summary: "",
-    offsetXMax: "300px",
-    offsetXMin: "-300px",
-    offsetYMax: "-200px",
-    offsetYMin: "200px"
+    author: "Various contributors",
+    summary:
+      "This is one of a series of periodicals published by The Creative Applications Network. \n\nThough my background does not cover Creative Technology, I still found this book inspiring and relevant to the work I do. Most people talk about emerging trends and where technology, science and art could go in the future. This book, however, actually demonstrates that with real projects, built by technologists all over the word.",
+    tags: ["Creative Technology", "Emerging Trends"],
+    reference: "https://creativeappsnet.myshopify.com/products/holo-2",
+    offsets: {
+      xMax: "300px",
+      xMin: "-300px",
+      yMax: "-200px",
+      yMin: "200px"
+    }
   },
   {
     id: "hello-robot",
     name: "Hello Robot",
-    summary: "",
-    offsetXMax: "200px",
-    offsetXMin: "0px",
-    offsetYMax: "0px",
-    offsetYMin: "-200px"
+    author: "Vitra Design Museum",
+    tags: ["Human-AI Collaboration", "Robotics", "Interaction Design"],
+    summary:
+      "I liked this book because it asks provoking questions and explores human-machine collaboration through a variety of lenses. \n\nIt introduced me to several new artists and creators working in this space and boasts an extensive catalogue of projects, initiatives etc. I still use this book to reference their glossary, which covers the most important terms surrounding the topic of robotics.",
+    reference:
+      "http://shop.design-museum.de/en/Publications/Hello-Robot-Design-between-Human-and-Machine.html?listtype=search&searchparam=hello%2C%20robot",
+    offsets: {
+      xMax: "200px",
+      xMin: "0px",
+      yMax: "0px",
+      yMin: "-200px"
+    }
   },
   {
     id: "content-design",
     name: "Content Design",
-    summary: "",
-    offsetXMax: "50px",
-    offsetXMin: "0px",
-    offsetYMax: "0px",
-    offsetYMin: "-200px"
+    author: "Sarah Richards",
+    tags: ["Content Design", "Communication", "Methods "],
+    reference: "http://contentdesign.london/home/book/",
+    summary:
+      "I was super excited when a fellow Content Designer introduced me to this small but mighty book. \n\n It's one of the few resources which clearly (and effectively) articulates Content Design through real-world examples and practical techniques.",
+    offsets: {
+      xMax: "50px",
+      xMin: "0px",
+      yMax: "0px",
+      yMin: "-200px"
+    }
   },
   {
     id: "sprint",
     name: "Sprint",
-    summary: "",
-    offsetXMax: "100px",
-    offsetXMin: "-100px",
-    offsetYMax: "-300px",
-    offsetYMin: "300px"
+    author: "Jake Knapp",
+    summary:
+      "Incase you haven't guessed by now, I'm drawn to books which use simple language and practical examples which I can reference more than once or share with others.\n\nIt's safe to say Sprint is one of those resources.\n\nIf you are looking to create a new venture or develop an existing product or company, read this book. Equally, if you are facilitating client sessions or helping teams build out new ideas, this book will also be of huge benefit to you. ",
+    tags: ["Facilitation", "Ideation", "Design Thinking", "Startups"],
+    reference: "https://www.thesprintbook.com",
+    offsets: {
+      xMax: "100px",
+      xMin: "-100px",
+      yMax: "-300px",
+      yMin: "300px"
+    }
   },
   {
     id: "the-laws-of-simplicity",
     name: "The Laws of Simplicity",
-    summary: "",
-    offsetXMax: "50px",
-    offsetXMin: "-50px",
-    offsetYMax: "200px",
-    offsetYMin: "0px"
+    author: "John Maeda",
+    tags: ["Design Principles", "Interaction Design", "Technology "],
+    summary:
+      "I found this book interesting because it put things into perspective for me.\n\nIt's easy to forget how fast technology has advanced in the last 15 - 20 years alone. \n\nI liked this book because it captures those key milestones, but grounds you with 10 timeless laws or principles which we can continue to consider when designing new products, services or features.",
+    reference: "http://lawsofsimplicity.com",
+    offsets: {
+      xMax: "50px",
+      xMin: "-50px",
+      yMax: "200px",
+      yMin: "0px"
+    }
   },
   {
     id: "make",
-    name: "Make",
-    summary: "",
-    offsetXMax: "70px",
-    offsetXMin: "-70px",
-    offsetYMax: "200px",
-    offsetYMin: "0px"
+    name: "Make: Wearable Electronics",
+    author: "Kate Hartman",
+    tags: ["Wearable Electronics", "Physical Computing", "Prototyping"],
+    summary:
+      "Up until reading this book, I had no experience with wearable technology but wanted to incorporate it into a prototype I was building.\n\nThe step-by-step instructions and large images make the subject really approachable and easy to follow. It mainly covers electronic circuits and sensors, but also shows how you can embed them into any type of clothing or apparel. ",
+    reference: "https://www.makershed.com/products/make-wearable-electronics",
+    offsets: {
+      xMax: "70px",
+      xMin: "-70px",
+      yMax: "200px",
+      yMin: "0px"
+    }
   },
   {
     id: "the-circle",
     name: "The Circle",
-    summary: "",
-    offsetXMax: "-300px",
-    offsetXMin: "300px",
-    offsetYMax: "300px",
-    offsetYMin: "-20px"
+    author: "Dave Eggers",
+    tags: ["Technology", "Fiction"],
+    summary:
+      "This book is a little different to things I normally read, but it got me thinking about how technology will continue to shape and influence our lives.\n\nWithout giving the plot away....it tells the story of Mae, a young woman who has just bagged herself an impressive job in the worlds most powerful internet company in California. \n\nIt's got quite a negative outlook on the future of identity, privacy, transparency etc, though I do think it's important to read if you work in technology, business or design.",
+    reference:
+      "https://books.google.ie/books?id=5VZ5AAAAQBAJ&printsec=frontcover&dq=the+circle&hl=en&sa=X&ved=0ahUKEwjC_KGDgK3aAhUID5AKHVyrBkwQ6AEIKTAA#v=onepage&q=the%20circle&f=false",
+    offsets: {
+      xMax: "-300px",
+      xMin: "300px",
+      yMax: "300px",
+      yMin: "-20px"
+    }
   },
   {
     id: "superintelligence",
-    name: "Superintelligence",
-    summary: "",
-    offsetXMax: "50px",
-    offsetXMin: "-50px",
-    offsetYMax: "-500px",
-    offsetYMin: "0px"
+    name: "Superintelligence: Paths, Dangers, Stragegies",
+    author: "Nick Bostrom",
+    tags: ["Evolution", "Cognitive Enhancement", "Artificial Intelligence"],
+    summary:
+      "The first thing I'll say about this book is it's pretty heavy on science and technology, so some of the terminology can be confusing if you're not overly familiar with AI or Cognitive Science. \n\nIt is however pretty helpful in terms of moving beyond the hype of AI and debunking some misconceptions about it's current capabilities. It's a little dystopian, but important to read if you are a designer, developer or engineer working with AI. ",
+    reference:
+      "https://books.google.ie/books?id=7_H8AwAAQBAJ&printsec=frontcover&dq=superintelligence+nick+bostrom&hl=en&sa=X&ved=0ahUKEwjHmfPcgK3aAhXSqZAKHQEFA-EQ6AEIKTAA#v=onepage&q=superintelligence%20nick%20bostrom&f=false",
+    offsets: {
+      xMax: "50px",
+      xMin: "-50px",
+      yMax: "-500px",
+      yMin: "0px"
+    }
   },
   {
     id: "practical-empathy",
     name: "Practical Empathy",
-    summary: "",
-    offsetXMax: "100px",
-    offsetXMin: "0px",
-    offsetYMax: "-300px",
-    offsetYMin: "100px"
+    author: "Indi Young",
+    tags: ["Strategy", "Behavioural Science", "Collaboration  "],
+    summary:
+      "This book made me think about empathy as a mindset and how to understand different perspectives or behaviours amongst individuals. \n\nI think it's really useful if you are trying to build out new strategies or become a more effective collaborator or team mate in general. ",
+    reference:
+      "https://www.amazon.com/Indi-Young/e/B001JP01C2/ref=dp_byline_cont_pop_book_1",
+    offsets: {
+      xMax: "100px",
+      xMin: "0px",
+      yMax: "-300px",
+      yMin: "100px"
+    }
   },
   {
     id: "market-cafe-magazine",
-    name: "Market Cafe Magazine",
-    summary: "",
-    offsetXMax: "200px",
-    offsetXMin: "0px",
-    offsetYMax: "0px",
-    offsetYMin: "0px"
+    name: "Market Cafe Magazine: Issue 02",
+    author: "Various contributors",
+    tags: ["Data Visualisation", "Data Science", "Information"],
+    summary:
+      "I really enjoyed this zine, even though I didn't fully understand the data behind all of the visualisations featured here. \n\nIt was nice to see different styles and opinions about what designing information means wand where its going. Also pretty cool to note this zine is 100% independent.",
+    reference:
+      "http://marketcafemag.bigcartel.com/product/market-cafe-magazine-issue-2",
+    offsets: {
+      xMax: "200px",
+      xMin: "0px",
+      yMax: "0px",
+      yMin: "0px"
+    }
   },
   {
     id: "cartographics",
-    name: "Cartographics",
-    summary: "",
-    offsetXMax: "100px",
-    offsetXMin: "0px",
-    offsetYMax: "0px",
-    offsetYMin: "0px"
+    name: "Cartographic's",
+    author: "Shan Ben Tu Shu",
+    tags: ["Cartographics", "Infographics", "Geography "],
+    summary:
+      "I've always been fascinated by maps, but will admit that Cartography was an alien word to me until reading this book. \n\nWe often take them for granted and don't stop to consider the amount of work that goes into mapping out environments, forms, activities and so on. I think this book will definitely inspire those who are curious about urbanisation, art and technological innovation.",
+    reference:
+      "https://books.google.ie/books?id=nKWpDAEACAAJ&dq=cartographics&hl=en&sa=X&ved=0ahUKEwj358Xqg63aAhUIS5AKHWDzBiwQ6AEIKTAA  ",
+    offsets: {
+      xMax: "100px",
+      xMin: "0px",
+      yMax: "0px",
+      yMin: "0px"
+    }
   },
   {
     id: "thinking-fast-and-slow",
     name: "Thinking Fast and Slow",
-    summary: "",
-    offsetXMax: "-20px",
-    offsetXMin: "0px",
-    offsetYMax: "20px",
-    offsetYMin: "0px"
+    author: "Daniel Kahneman",
+    tags: ["Behavioural Science", "Cognitive Science", "Psychology "],
+    summary:
+      "This is a book about making choices and the difference between those that are intuitive v's rational. \n\nI always presumed that the decisions I made were within my control but after reading this book, I learned that's not always the case as our minds can be tripped up. It's definitely relevant if you want to lean how to control your thoughts or improve your ability to make such decisions.",
+    reference:
+      "https://books.google.ie/books?id=oV1tXT3HigoC&dq=thinking+fast+and+slow&source=gbs_navlinks_s",
+    offsets: {
+      xMax: "-20px",
+      xMin: "0px",
+      yMax: "20px",
+      yMin: "0px"
+    }
   },
   {
     id: "linchpin",
     name: "Linchpin",
-    summary: "",
-    offsetXMax: "200px",
-    offsetXMin: "0px",
-    offsetYMax: "0px",
-    offsetYMin: "0px"
+    author: "Seth Godin",
+    tags: ["Self-development", "Confidence", "Career Advice"],
+    summary:
+      "This is one of those books that you either love or hate as the advice is quite one-sided in my opinion anyway. \n\nIt talks about the increasing pressure to continually upskill at work in order to stay relevant or competitive but also advises on how to create your unique own role and essentially, become indispensable.",
+    reference:
+      "https://books.google.ie/books?id=eqrFTyReiOwC&printsec=frontcover&dq=linchpin&hl=en&sa=X&ved=0ahUKEwiyyLqV_6zaAhVIg5AKHccWApEQ6AEIKTAA#v=onepage&q=linchpin&f=false",
+    offsets: {
+      xMax: "200px",
+      xMin: "0px",
+      yMax: "0px",
+      yMin: "0px"
+    }
   },
   {
     id: "mindset",
     name: "Mindset",
-    summary: "",
-    offsetXMax: "0px",
-    offsetXMin: "200px",
-    offsetYMax: "-500px",
-    offsetYMin: "0px"
+    author: "Dr. Carol S. Dweck",
+    tags: ["Psychology", "Self-fulfillment", "Personal Development "],
+    summary:
+      "This book is based on decades of research on achievement and success so is helpful if you are trying to get motivated or set some new life goals. \n\nIt focuses less on achievements or accomplishments and more on developing the right mindset and the science behind it. ",
+    reference:
+      "https://books.google.ie/books/about/Mindset.html?id=ckoKDQAAQBAJ&redir_esc=y",
+    offsets: {
+      xMax: "0px",
+      xMin: "200px",
+      yMax: "-500px",
+      yMin: "0px"
+    }
   },
   {
     id: "business-model-generation",
     name: "Business Model Generation",
-    summary: "",
-    offsetXMax: "0px",
-    offsetXMin: "-50px",
-    offsetYMax: "0px",
-    offsetYMin: "-400px"
-  }, {
+    author: "Alexander Osterwalder and Yves Pigneur",
+    tags: ["Business Design", "Venture Design", "Entrepreneurship", "Strategy"],
+    summary:
+      "I've had this book for a year or so now and I still continue to reference it at work. \n\nI like it because it provides an extensive collection of case studies and Business Model patterns, but it also talks about the approach - what model to apply to your strategy and when. \n\nIt's super practical and also offers additional templates and resources online which is particularly useful if you run workshops or client sessions on a regular basis.",
+    reference: "https://strategyzer.com/books/business-model-generation",
+    offsets: {
+      xMax: "0px",
+      xMin: "-50px",
+      yMax: "0px",
+      yMin: "-400px"
+    }
+  },
+  {
     id: "where-good-ideas-come-from",
     name: "Where Good Ideas Come From",
-    summary: "",
-    offsetXMax: "0px",
-    offsetXMin: "0px",
-    offsetYMax: "-400px",
-    offsetYMin: "200px"
-  }, {
+    author: "Steven Johnson",
+    tags: ["Innovation", "Entrepreneurship"],
+    summary:
+      "This book helped me understand the conditions in which innovation thrives.\n\nIt maps out major breakthroughs which have occurred throughout history and then helps you to see how such ideas are connected or intertwined. I liked this study because it moved beyond the idea alone and considered environments, society, cultures, industries etc.",
+    reference:
+      "https://books.google.ie/books?id=0hjEuFB9oTkC&dq=where+good+ideas+come+from&hl=en&sa=X&ved=0ahUKEwi9hIT__qzaAhWBqZAKHdQRBWkQ6AEINDAC",
+    offsets: {
+      xMax: "0px",
+      xMin: "0px",
+      yMax: "-400px",
+      yMin: "200px"
+    }
+  },
+  {
     id: "sapiens",
     name: "Sapiens",
-    summary: "",
-    offsetXMax: "200px",
-    offsetXMin: "0px",
-    offsetYMax: "-100px",
-    offsetYMin: "0px"
-  }, {
+    author: "Yuval noah Harari",
+    tags: ["Biology", "History", "Cognitive Development"],
+    summary:
+      "This book blew my mind, but it's not for those looking for a light read. \n\nWhen I first starting reading this book, I was expecting to learn about the evolution of animals and humans backed by lots of facts and numbers. I definitely got that, but it was told in more of a story and covered all sorts of other behaviour-related subjects such as power, capitalism,  religion and more. \n\nReally enjoyed reading this, but it takes time to absorb all the information - if I was to go back again, I'd definitely purchase the Audio Book not the paper back.",
+    reference: "http://www.ynharari.com/book/sapiens/",
+    offsets: {
+      xMax: "200px",
+      xMin: "0px",
+      yMax: "-100px",
+      yMin: "0px"
+    }
+  },
+  {
     id: "the-design-of-business",
     name: "The Design of Business",
-    summary: "",
-    offsetXMax: "100px",
-    offsetXMin: "0px",
-    offsetYMax: "-200px",
-    offsetYMin: "0px"
-  }, {
+    author: "Roger Martin",
+    tags: ["Design Thinking", "Innovation", "Strategy"],
+    summary:
+      "I enjoyed this book because it's honest about the problems large organisations experience when they are trying to implement change programs or promote a culture of intrapreneurship and experimentation.  \n\nIt's highly relevant for those in the field of Business or Organisation Design, but is definitely not confined to these disciplines. I think it gives Design Thinking the credit it deserves and I found the case studies really helpful in setting the context.",
+    reference:
+      "https://www.goodreads.com/book/show/6552612-the-design-of-business",
+    offsets: {
+      xMax: "100px",
+      xMin: "0px",
+      yMax: "-200px",
+      yMin: "0px"
+    }
+  },
+  {
     id: "creative-confidence",
     name: "Creative Confidence",
-    summary: "",
-    offsetXMax: "100px",
-    offsetXMin: "0px",
-    offsetYMax: "-150px",
-    offsetYMin: "0px"
-  }, {
+    author: "Tom Kelly, David Kelly",
+    tags: ["Personal Growth", "Creativity", "Storytelling"],
+    summary:
+      "There's been a lot of hype around this book, so I was a little hesitant about it initially. \n\nThe first thing that really stuck me about it was the narrative - it's inclusive, relatable and filled with short stories and lessons throughout. I think this would appeal to students or professionals of any background and it's something you can pick up and read on-the-fly. \n\nIt definitely changed my perspective and pushed me to adapt a more creative mindset, especially at work.",
+    reference: "https://www.creativeconfidence.com",
+    offsets: {
+      xMax: "100px",
+      xMin: "0px",
+      yMax: "-150px",
+      yMin: "0px"
+    }
+  },
+  {
     id: "this-is-service-design-thinking",
     name: "This is service Design Thinking",
-    summary: "",
-    offsetXMax: "-100px",
-    offsetXMin: "0px",
-    offsetYMax: "50px",
-    offsetYMin: "0px"
+    author: "Various contributors",
+    tags: ["Service Design", "User-Centered Design", "Process"],
+    summary:
+      "Like so many of the other books listed on my site, this one is more of a practical handbook which I use on an ongoing basis. \n\nIt's suitable for different levels of experience and shares methods and insights from well established Service Designers around the world.",
+    reference: "http://thisisservicedesignthinking.com",
+    offsets: {
+      xMax: "-100px",
+      xMin: "0px",
+      yMax: "50px",
+      yMin: "0px"
+    }
   }
 ];
