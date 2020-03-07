b93f93d6ed5f488d97282be6ae274f84e37c1463
Eibhlin <eibhlin@blstr.co>
2020-03-07 15:56:55 +0000

    refactor: Create top five articles page with stateful component wrapper

---
+++ src/pages/TopFiveArticles/index.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import PageTitle from '../../components/PageTitle'
+import ContentLayout from '../../components/ContentLayout'
+import StatefulComponent from '../StatefulComponent'
+import { formatContentfulDate } from '../../utils'
+import contentful from '../../clients/contentful'
+
+import './index.scss'
+
+const sortByWeekStarting = data =>
+  data.sort((a, b) => new Date(b.weekStarting) - new Date(a.weekStarting))
+
+const TopFiveArticles = ({ title }) => {
+  return (
+    <div className="TopFive">
+      <PageTitle text={title} />
+      <StatefulComponent
+        fetchData={contentful.getTopFiveArticles}
+        sortData={sortByWeekStarting}
+      >
+        <Content />
+      </StatefulComponent>
+    </div>
+  )
+}
+
+export default TopFiveArticles
+
+const Content = ({ data }) =>
+  data.map(({ weekStarting, articles: weeklyArticles }, index) => (
+    <ContentLayout
+      key={index}
+      subtitle={formatContentfulDate(weekStarting)}
+      highlightOnHover
+      content={weeklyArticles.map(
+        ({ fields: { url, external, label } }, index) => (
+          <a
+            href={url}
+            className="TopFive-link"
+            {...(external
+              ? { target: '_blank', rel: 'noopener noreferrer' }
+              : {})}
+          >
+            {label}
+          </a>
+        )
+      )}
+    />
+  ))
