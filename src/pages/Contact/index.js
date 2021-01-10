86c08c63cd193d33d1db4a23c8b2dfa134ac0032
Eibhlin <eibhlin@blstr.co>
2021-01-10 16:50:06 +0000

    update contact page

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,20 +1,15 @@
-import React, { Component } from "react";
-import PageTitle from "../../components/PageTitle";
-import ContentLayout from "../../components/ContentLayout";
-import "./index.scss";
+import React, { Component } from 'react'
+import PageTitle from '../../components/PageTitle'
+import ContentLayout from '../../components/ContentLayout'
+import './index.scss'
 
 class Contact extends Component {
   generateLink(name, url) {
     return (
-      <a
-        href={url}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="Contact-link"
-      >
+      <a href={url} target="_blank" rel="noopener noreferrer" className="Contact-link">
         {name}
       </a>
-    );
+    )
   }
   render() {
     return (
@@ -27,46 +22,36 @@ class Contact extends Component {
               {`If you are a mysterious prince who wants to offer me a lot of`}
               <br />
               {`money, you can spam me via email: `}
-              {this.generateLink(
-                "delany.design@gmail.com",
-                "mailto:delany.design@gmail.com"
-              )}
+              {this.generateLink('delany.design@gmail.com', 'mailto:delany.design@gmail.com')}
             </span>,
             <span className="Contact-paragraph">
-              {`If you enjoy looking at random photos of rubbish, moments in`}
+              {`If you're curious to know how I apply my skills outside the workplace,`}
               <br />
-              {`time or nature, you can follow me on `}
-              {this.generateLink(
-                "Instagram",
-                "https://www.instagram.com/eilis_delany/"
-              )}
-              {"."}
+              {`you can check out this `}
+              {this.generateLink('side project', 'https://www.instagram.com/project_eire/')}
+              {`, created in collaboration with some `}
+              {this.generateLink('mighty-fine friends', 'https://www.instagram.com/p/CIF969jjIHI/')}
+              {`.`}
             </span>,
             <span className="Contact-paragraph">
-              {`If you are a master of multiple tabs and want to read my work`}
+              {`If you're a master of multiple tabs and want to read my work`}
               <br />
               {`on alternative platforms, you can find me on `}
-              {this.generateLink("Medium", "https://medium.com/@delany.design")}
-              {"."}
+              {this.generateLink('Medium', 'https://medium.com/@delany.design')}
+              {'.'}
             </span>,
             <span className="Contact-paragraph">
-              {`If you want me to endorse your Microsoft Office skills or`}
+              {`If you'd like me to endorse your Microsoft Office skills`}
               <br />
-              {`sponsor an upcoming event, you can connect with me on `}
-              {this.generateLink(
-                "LinkedIn",
-                "https://www.linkedin.com/in/eilisdelany/"
-              )}
-
-              {"."}
+              {`you can connect with me on `}
+              {this.generateLink('LinkedIn', 'https://www.linkedin.com/in/eilisdelany/')}
+              {'.'}
             </span>,
-            <span>
-              {"If you believe you’re the love of my life, keep searching…."}
-            </span>
+            <span>{'If you believe you’re the love of my life, keep searching….'}</span>,
           ]}
         />
       </div>
-    );
+    )
   }
 }
-export default Contact;
+export default Contact
